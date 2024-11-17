914995a84b0fb03badbd7738b05680b7a868fb07
Rafa130206 <rafabompadre@gmail.com>
2024-11-17 15:29:51 -0300

    Cabecalho criado

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
 import "./globals.css";
+import Cabecalho from "./components/Cabecalho";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return(
     <html lang="pt-br">
-      <body>
+      <body className="flex flex-col min-h-screen">
+        <Cabecalho/>
         {children}
       </body>
     </html>
