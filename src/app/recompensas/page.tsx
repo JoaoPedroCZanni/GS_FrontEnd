c22d306792d27c43acc5c63a9b4c8fb6d34f67b4
Rafa130206 <rafabompadre@gmail.com>
2024-11-18 23:51:40 -0300

    Formulario de recompensas criado

--- src/app/recompensas/page.tsx
+++ src/app/recompensas/page.tsx
@@ -3,7 +3,7 @@ export default function Atividades() {
         <div className="flex flex-col items-center">
             <h1 className="text-center my-6 pt-2 text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold">Atividades</h1>
 
-            <button className="text-center text-lg bg-green-600 hover:bg-green-700 text-white font-bold p-3 rounded-lg m-12"><a href="/atividades/adicionar-recompensa">Adicionar recompensa</a></button>
+            <button className="text-center text-lg bg-green-600 hover:bg-green-700 text-white font-bold p-3 rounded-lg m-12"><a href="/recompensas/adicionar-recompensa">Adicionar recompensa</a></button>
         </div>
     )
 }
