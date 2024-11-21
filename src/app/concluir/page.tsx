cdb7e320192ec9ba8d71fa4690dbe0ffa77aec13
Rafa130206 <rafabompadre@gmail.com>
2024-11-21 14:51:16 -0300

    Página criada

--- src/app/concluir/page.tsx
+++ src/app/concluir/page.tsx
@@ -1,7 +1,31 @@
+"use client"
+import { Toaster, toast } from "react-hot-toast";
+import { TipoEvidenvia } from "../types";
+import { useEffect, useState } from "react";
+
 export default function Concluir() {
+    const [atividades, setAtividades] = useState<TipoEvidenvia[]>([])
+
+    const consumindoApiAtividades = async () => {
+        try {
+            const response = await fetch('http://localhost:3000/api/base-evidencias')
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const data = await response.json()
+            setAtividades(data)
+        } catch (error) {
+            console.log("Erro ao buscar evidências:", error)
+        }
+    }
+
+    useEffect(() => {
+        consumindoApiAtividades()
+    }, [])
+
     return (
         <div>
-            
+            <h1 className="text-center my-6 pt-2 text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold">Conclua uma Atividade!</h1>
         </div>
     )
 }
